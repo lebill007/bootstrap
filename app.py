da0b1c99ee1c0cd175d63781bf953bcb07ad72a9
DPEGEIC <t-323289@senacsp.edu.br>
2023-05-09 11:51:10 -0300

    novo arquivo base

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ from flask import Flask, template_rendered,request
 
 app = Flask(__name__)
 
+#rota para a pagina inicial da aplicação
 @app.route('/')
 def index():
     return "residente evil 4 é o melhor remake"
 
-if __name__ == '__main__':
-    app.run(debug=True)
-        
