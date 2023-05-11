6eac95432a92eb851b3c53a0c6feafad07926c68
DPEGEIC <t-323289@senacsp.edu.br>
2023-05-11 11:49:18 -0300

    menus

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask, template_rendered,request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 #rota para a pagina inicial da aplicação
 @app.route('/')
 def index():
-    return "residente evil 4 é o melhor remake"
+    return render_template("index.html")
 
+#rota para contato
+@app.route("/contato")
+def contato():
+    return render_template('contato.html')
+
+#rota para cursos
+@app.route('/cursos')
+def cursos():
+    return render_template("cursos.html")
+
+#rota para login
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+#rota para ead
+@app.route('/sobre_ead')
+def sobre_o_ead():
+    return render_template('sobre_ead.html')
+
+if __name__== '__main__':
+    app.run(debug=True)
+    
